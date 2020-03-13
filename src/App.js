a66da65d2f21340c3e0c6428095b8277fa18a39e
Nick Kebbas <nkebbas@usfca.edu>
2020-03-13 15:02:14 -0700

    implement yelp api call but running into CORS error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import React from 'react';
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
+import authProvider from './authProvider';
 import { UserList } from './users';
+import yelpProvider from "./yelpProvider";
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-     <Admin dataProvider={dataProvider}>
+     <Admin dataProvider={yelpProvider} authProvider={authProvider}>
         <Resource name="users" list={UserList} />
+        <Resource name="restaurants" list={ListGuesser} />
      </Admin>
 );
 export default App;
